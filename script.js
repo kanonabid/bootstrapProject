859ab7597e6e91bc60c54ae9adbc97c52608187c
Md. Mostafijur Rahman <kanonabid1@gmail.com>
2024-11-26 12:12:19 +0600

    contact form

---
+++ script.js
@@ -0,0 +1,43 @@
+// function test(){
+//     console.log("Hello World");
+//     var nums1 = [1, 2, 3, 4, 5];
+//     var k = 2
+//     const revNums=(nums, start, end)=>{
+//     while(start < end){
+//         [nums[start], nums[end]]=[nums[end],nums[start]];
+//         start++;
+//         end--;
+//     }
+//     };
+
+//     var rotate = function(nums, k){
+//     k = k % nums.length;
+
+//     nums.reverse();
+//     revNums(nums1, 0, k-1);
+//     var result= revNums(nums, k, nums.length-1);
+//     };
+    
+//     }
+
+
+
+
+    // function rotateArray(arr, k){
+    //     for (let i = 0; i < k; i++){
+    //         let temp = arr[0];
+    //         for (let j = 0; j < arr.length; j++){
+    //             arr[j] = arr[j+1];
+    //         };
+    //         arr[arr.length - 1] = temp;
+    //     }
+    //     return arr
+        
+    // };
+    
+    // let arr=[1, 2, 3, 4, 5];
+    // let k = 3;
+    // console.log(rotateArray(arr, k))
+
+
+
